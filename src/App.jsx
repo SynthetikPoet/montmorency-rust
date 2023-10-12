23698c8b136c8a47a4bf70738e4c727a776b68b9
Antonii Spasov <spasovantonii5@gmail.com>
2023-10-12 10:13:37 -0400

    Fait: Routage basique

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  
+
   return (
     <>
       <div>
-        <h1>
-          Rust!
-        </h1>
+        Hello!
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import App from './App'
+import ErrorPage from './ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+    errorElement: <ErrorPage/>,
+    children:[
+      {
+        path:"about",
+        element: <h1>Rust!</h1>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
